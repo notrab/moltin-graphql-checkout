5776cbc6c3a956948e1f54fb87ca15c55d6850d9
Jamie Barton <hi@jamiebarton.co.uk>
2018-03-24 13:37:34 +0000

    Added main image to resolver response

--- src/resolvers/queries/products.ts
+++ src/resolvers/queries/products.ts
@@ -3,10 +3,25 @@ import { Context } from '../../utils'
 export default async (parent, args, ctx: Context, info) => {
   const { moltin } = ctx
 
-  const { json: { data } } = await moltin.Products.All()
+  const {
+    json: { data, included: { main_images } }
+  } = await moltin.Products.With(['main_image']).All()
+
+  const products = data.map(product => {
+    const imageId = product.relationships.main_image
+      ? product.relationships.main_image.data.id
+      : false
+
+    return {
+      ...product,
+      image: imageId
+        ? main_images.find(img => img.id === imageId).link.href
+        : null
+    }
+  })
 
   try {
-    return data
+    return products
   } catch ({ status, json }) {
     return {
       status,
